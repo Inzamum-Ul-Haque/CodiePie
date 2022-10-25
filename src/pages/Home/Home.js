81ab611982e021c5deae8eb96f57f40f43c12f11
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-25 23:24:10 +0600

    Home page designed and images imported

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>This is home</div>;
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 home-container">
+      <div className="p-24">
+        <h2 className="text-6xl pb-6 home-title">
+          <span className="yellow-shape">
+            <span className="text-sky-800 font-semibold">Codie</span>
+            <span className="text-amber-300 font-semibold">Pie</span>
+            <img
+              src="https://react-educal.netlify.app/assets/img/shape/yellow-bg.png"
+              alt=""
+            />
+          </span>{" "}
+          The best place to learn Web Development and more!!!
+        </h2>
+        <p className="home-text text-2xl">
+          Over 80+ courses related to web dev, client or frontend, server or
+          backend, database etc which are created by renowned teachers from
+          renowned institutes.
+        </p>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
