63cb7cbdbe94b512baaf6fe2fd0686c6873c1feb
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-26 01:41:56 +0600

    Saving commits

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,7 +1,66 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logos/codiepie-256.png";
+import "./Header.css";
 
 const Header = () => {
-  return <div>This is header</div>;
+  const activeStyle = {
+    fontWeight: "bold",
+  };
+
+  return (
+    <div>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="shadow p-3 mb-5 bg-body rounded"
+      >
+        <Container className="header-container">
+          <Navbar.Brand className="header-brand">
+            <Link className="name-logo" to="/">
+              <img src={logo} alt="" />
+              <span>
+                <span className="title-left">Codie</span>Pie
+              </span>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto nav-links">
+              <NavLink
+                className="center-nav"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                to="/courses"
+              >
+                Courses
+              </NavLink>
+              <NavLink
+                className="center-nav"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                to="/faq"
+              >
+                FAQ
+              </NavLink>
+              <NavLink
+                className="center-nav"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                to="/blog"
+              >
+                Blog
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+          <Link to="/signin">
+            <Button variant="outline-primary login-btn">Login</Button>
+          </Link>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Home.css";
+import homeImg from "../../assets/images/home-img.svg";
 
 const Home = () => {
-  return <div>This i home</div>;
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className="col" sm={12} lg={6}>
+            <div>
+              <h2 className=" home-title">
+                <span className="yellow-shape">
+                  <span className="span-left">Codie</span>
+                  <span className="span-right">Pie</span>
+                  <img
+                    src="https://react-educal.netlify.app/assets/img/shape/yellow-bg.png"
+                    alt=""
+                  />
+                </span>{" "}
+                The best platform to learn Web Development and more!!!
+              </h2>
+              <p className="home-text">
+                Over 80+ courses related to web dev, client or frontend, server
+                or backend, database etc which are created by renowned teachers
+                from renowned institutes.
+              </p>
+            </div>
+          </Col>
+          <Col sm={12} lg={6}>
+            <div className="img-container d-flex justify-content-center">
+              <img className="" src={homeImg} alt="" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
