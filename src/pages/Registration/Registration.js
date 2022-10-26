7927be3f718cdc2ad2f1768e13e5d2f74ec24fe8
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-26 11:47:40 +0600

    Added sign in and registration design

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,7 +1,77 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Registration.css";
 
 const Registration = () => {
-  return <div>This is registration.</div>;
+  return (
+    <div className="register-container">
+      <div>
+        <h2 className="text-center">Create a free account</h2>
+      </div>
+      <div className="form-container d-flex justify-content-center">
+        <Form className="bg-white border border-secondary">
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Control
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="Full name"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Control
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="photo URL"
+            />
+          </Form.Group>
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Control
+              className="form-control form-control-lg"
+              type="email"
+              placeholder="e-mail address"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Control
+              className="form-control form-control-lg"
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Control
+              className="form-control form-control-lg"
+              type="password"
+              placeholder="Re-type password"
+              required
+            />
+          </Form.Group>
+          <Form.Group
+            className="mb-4 d-flex justify-content-between"
+            controlId="formBasicCheckbox"
+          >
+            <Form.Check
+              type="checkbox"
+              label="I agree to the Terms and Conditions"
+            />
+          </Form.Group>
+          <Button className="w-100 signup-btn" variant="primary" type="submit">
+            Sign Up
+          </Button>
+          <p className="text-center mt-3">
+            Already in CodiePie ?{" "}
+            <Link to="/signin" className="back-to-signin-text">
+              Sign In
+            </Link>
+          </p>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,7 +1,67 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "./SignIn.css";
+import { FaGoogle, FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="signin-container">
+      <div>
+        <h2 className="text-center">Sign In to get access</h2>
+      </div>
+      <div className="form-container d-flex justify-content-center">
+        <Form className="bg-white border border-secondary">
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Control
+              className="form-control form-control-lg"
+              type="email"
+              placeholder="Username or Email"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-4" controlId="formBasicPassword">
+            <Form.Control
+              className="form-control form-control-lg"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Form.Group
+            className="mb-4 d-flex justify-content-between"
+            controlId="formBasicCheckbox"
+          >
+            <Form.Check type="checkbox" label="Remember me" />
+            <Link className="form-text">Forgot Password?</Link>
+          </Form.Group>
+          <Button className="w-100  signin-btn" variant="primary" type="submit">
+            Login
+          </Button>
+          <p className="text-center mt-2">
+            Don't have an account?{" "}
+            <span>
+              <Link to="/registration" className="form-text">
+                Create an account
+              </Link>
+            </span>
+          </p>
+          <div className="divider my-5">
+            <p>
+              <span>Or</span>
+            </p>
+          </div>
+          <div className="btn-third-party">
+            <Button className="w-100 mb-4">
+              <FaGoogle /> Continue with Google
+            </Button>
+            <Button className="w-100  ">
+              <FaGithub /> Continue with Github
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
